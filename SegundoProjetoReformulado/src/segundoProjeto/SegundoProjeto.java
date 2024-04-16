d716e3ee34d513b8575aea2b706dc6d9dfeaeba3
Robert  Alexander <delrobert.alexander@gmail.com>
2024-04-16 17:17:37 -0300

    Primeira Branch.

--- SegundoProjetoReformulado/src/segundoProjeto/SegundoProjeto.java
+++ SegundoProjetoReformulado/src/segundoProjeto/SegundoProjeto.java
@@ -2,6 +2,30 @@ package segundoProjeto;
 
 public class SegundoProjeto {
 	
+	private String nome;
+	private int idade;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
 	public static void main(String[] args) {
 		System.out.println("Olá, mundo!");
 	}
